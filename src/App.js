58e9ead1ad5e4867a70b5e2a1e70e75a7f4537c9
techsithgit <hemilp@tuplecorp.com>
2019-05-27 07:58:40 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import NameTag from "./components/NameTag";
+
+//instead of initin inline use ref
+
+//list
+//for loop wont work .  use map
+//errors with dynamically created list 
+//react needs to keep track of indexes
+//there are better ways to create unique ids
+//i wish Symbol would work but it doesnt
+//usually dont use index becase if you delete a row and add a new row.   
+// use something from the data to create an id
+//there are third party lib avail 
+const initNames = [
+  {
+    firstName: "john",
+    lastName: "johnson"
+  },
+  {
+    firstName: "peter",
+    lastName: "peterson"
+  },
+  {
+    firstName: "jill",
+    lastName: "jillson"
+  }
+];
 
 function App() {
+  const [names, setNames] = useState(initNames);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">Name List</h1>
+        {/* <NameTag firstName={names[0].firstName} lastName={names[0].lastName} />
+        <NameTag firstName={names[1].firstName} lastName={names[1].lastName} />
+        <NameTag firstName={names[2].firstName} lastName={names[2].lastName} /> */}
+
+        {names.map((v, i) => {
+          return (
+            <NameTag key={`${i}{v.firstName}{v.lastName}`} firstName={v.firstName} lastName={v.lastName} />
+          );
+        })}
       </header>
     </div>
   );
